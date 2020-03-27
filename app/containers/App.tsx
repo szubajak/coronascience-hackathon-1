cf3d82b6ad1c4c6ed21ff3ce641e361e1c7d48c1
Olivier Descloux <olivier.descloux@bfh.ch>
2020-03-27 17:15:04 +0100

    Fix merge issues

--- app/containers/App.tsx
+++ app/containers/App.tsx
@@ -1,124 +1,111 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import {store, persistor} from '../store';
-import Login from './Login';
+import Impressum from './Impressum';
 import Dashboard from './Dashboard';
 import Informations from './Informations';
 import Settings from './Settings'
-import Impressum from './Impressum'
-import Symptom from './Symptom'
 import Profile from './Profile'
+import Symptom from './Symptom'
 import Icon from 'react-native-vector-icons/AntDesign';
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { colors } from '../styles/App.style';
+import { PersistGate } from 'redux-persist/integration/react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 const RootStack = createStackNavigator();
-const Tab = createBottomTabNavigator();
-
-function mainStack(){
-import { PersistGate } from 'redux-persist/integration/react';
-
 const Tab = createBottomTabNavigator();
 
 function Root() {
   return (
     <>
     <Tab.Navigator
-        tabBarOptions={{
-          activeTintColor: colors.secondaryDark,
-          inactiveTintColor: 'gray',
-          showLabel: false,
-          adaptive: true,
+      tabBarOptions={{
+        activeTintColor: colors.secondaryDark,
+        inactiveTintColor: 'gray',
+        showLabel: false,
+        adaptive: true,
+      }}
+    >
+      <Tab.Screen
+        name="Dashboard"
+        component={Dashboard}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name='home' color={color} size={size} />
+          )
+        }}
+      />
+
+      <Tab.Screen
+        name="Informations"
+        component={Informations}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name='info' color={color} size={size} />
+          )
+        }}
+      />
+
+      <Tab.Screen
+        name="AddSymptoms"
+        component={Symptom}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name='plussquare' color={color} size={size} />
+          )
+        }}
+      />
+
+      <Tab.Screen
+        name="Settings"
+        
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name='setting' color={color} size={size} />
+          )
         }}
       >
-        <Tab.Screen
-          name="Dashboard"
-          component={Dashboard}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <Icon name='home' color={color} size={size} />
-            )
-          }}
-        />
+          {props => <Settings {...props} />}
+      </Tab.Screen>
 
-        <Tab.Screen
-          name="Informations"
-          component={Informations}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <Icon name='info' color={color} size={size} />
-            )
-          }}
-        />
-
-        <Tab.Screen
-          name="AddSymptoms"
-          component={Symptom}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <Icon name='plussquare' color={color} size={size} />
-            )
-          }}
-        />
-
-        <Tab.Screen
-          name="Settings"
-          
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <Icon name='setting' color={color} size={size} />
-            )
-          }}
-        >
-           {props => <Settings {...props} />}
-        </Tab.Screen>
-
-        <Tab.Screen
-          name="Profile"
-          component={Profile}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <Icon name='user' color={color} size={size} />
-            )
-          }}
-        />
-      </Tab.Navigator>
-      </>
-  )
-}
-
-export default function App() {
-  return (
-    <>
-    <NavigationContainer>
-      <RootStack.Navigator mode="modal">
-        <RootStack.Screen
-          name="Main"
-          component={mainStack}
-          options={{ headerShown: false }}
-        />
-        <RootStack.Screen 
-          name="Impressum" 
-          component={Impressum} 
-          options={{ headerShown: false }} // TODO: fix header layout bug "...TransitionPresets.DefaultTransition"
-
-        />
-      </RootStack.Navigator>
-    </NavigationContainer>
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name='user' color={color} size={size} />
+          )
+        }}
+      />
+    </Tab.Navigator>
     </>
   );
 }
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <PersistGate
-        persistor={persistor}>
-        <Root/>
-      </PersistGate>
-    </Provider>
+    <>
+      <Provider store={store}>
+        <PersistGate
+          persistor={persistor}>
+          <NavigationContainer>
+            <RootStack.Navigator mode="modal">
+              <RootStack.Screen
+                name="Main"
+                component={Root}
+                options={{ headerShown: false }}
+              />
+              <RootStack.Screen 
+                name="Impressum" 
+                component={Impressum} 
+                options={{ headerShown: false }} // TODO: fix header layout bug "...TransitionPresets.DefaultTransition"
+              />
+            </RootStack.Navigator>
+          </NavigationContainer>
+        </PersistGate>
+      </Provider>
+    </>
   );
 }
