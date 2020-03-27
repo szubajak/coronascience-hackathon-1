37892c3b71fba426c2f58f5a9471913125d59801
Olivier Descloux <olivier.descloux@bfh.ch>
2020-03-27 18:27:15 +0100

    Add settings en transtallation

--- app/containers/Settings.tsx
+++ app/containers/Settings.tsx
@@ -43,13 +43,13 @@ class Settings extends Component<PropsType, State> {
         <StatusBar barStyle="dark-content" />
         <SafeAreaView style={{ flex: 0, backgroundColor: colors.headerGradientEnd }} />
         <SafeAreaView style={{ flex: 1, backgroundColor: colors.white }}>
-            <HeaderBanner title='Einstellungen'/>
+            <HeaderBanner title={localeString('settings.title')}/>
             <ScrollView 
                 style={{height: '100%', marginLeft:'10%', marginRight:'10%'}}
                 contentInsetAdjustmentBehavior="automatic">
                 <View>
                     <Text style={[AppStyle.sectionTitle]}>
-                        Benutzeroberfläche
+                        {localeString('settings.userInterface.title')}
                     </Text>
                 </View>
                 <Separator/>
@@ -66,16 +66,18 @@ class Settings extends Component<PropsType, State> {
                 </View>
                 <View>
                     <Text style={[AppStyle.sectionTitle,{marginBottom: 5}]}>
-                        Erinnerungen
+                        {localeString('settings.notifications.title')}
                     </Text>
                     <Text style={[AppStyle.textQuestion]}>
-                        Erinnerungen werden nur aktiviert, wenn im gewählten Intervall keine Symptome erfasst wurde.
+                      {localeString('settings.notifications.notificationsInformation')}
                     </Text>
                 </View>
 
                 <ListItem noIndent>
                   <Left>
-                    <Text>Erinnerungen aktivieren</Text>
+                    <Text>
+                      {localeString('settings.notifications.notificationActivation')}
+                    </Text>
                   </Left>
                   <Right>
                     <Switch/>
@@ -88,7 +90,7 @@ class Settings extends Component<PropsType, State> {
                 
                 <View>
                     <Text style={[AppStyle.sectionTitle,{marginBottom: 5}]}>
-                        Weiteres
+                      {localeString('settings.other.title')}
                     </Text>
                     <Separator />
 
@@ -97,24 +99,24 @@ class Settings extends Component<PropsType, State> {
                     <Col style={[styles.columns]}>
                       <Button style={[AppStyle.button]}
                         onPress={() => this.goToImpressum()} >
-                        <Text style={[AppStyle.textButton]}>Impressum</Text>
+                        <Text style={[AppStyle.textButton]}>{localeString('settings.other.impressum')}</Text>
                       </Button>
                     </Col>
                     <Col style={[styles.columns]}>
                       <Button style={[AppStyle.button]}>
-                        <Text style={[AppStyle.textButton]}>Nutzungsbedingungen</Text>
+                        <Text style={[AppStyle.textButton]}>{localeString('settings.other.termsOfUse')}</Text>
                       </Button>
                     </Col>
                   </Row>
                   <Row>
                     <Col style={[styles.columns]}>
                       <Button style={[AppStyle.button]}>
-                        <Text style={[AppStyle.textButton]}>Datenschutzerklärung</Text>
+                        <Text style={[AppStyle.textButton]}>{localeString('settings.other.privacyPolicy')}</Text>
                       </Button>
                     </Col>
                     <Col style={[styles.columns]}>
                       <Button style={[AppStyle.button]}>
-                        <Text style={[AppStyle.textButton]}>App Feedback</Text>
+                        <Text style={[AppStyle.textButton]}>{localeString('settings.other.feedback')}</Text>
                       </Button>
                     </Col>
                   </Row>
@@ -124,7 +126,7 @@ class Settings extends Component<PropsType, State> {
                 <View>
                   <Button style={[AppStyle.button, {backgroundColor:colors.secondaryNormal}]}>
                     <Text style={[AppStyle.textButton,{color:colors.white}]}>
-                      Ich möchte spenden!
+                      {localeString('settings.donate')}
                     </Text>
                   </Button>
                 </View>
