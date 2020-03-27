dcc05859cf1ee0adcf157016e804e8f54a74f13b
Olivier Descloux <olivier.descloux@bfh.ch>
2020-03-27 18:39:49 +0100

    fix merge error -> setting screen with bad closing tag

--- app/containers/Settings.tsx
+++ app/containers/Settings.tsx
@@ -42,99 +42,87 @@ class Settings extends Component<PropsType, State> {
       <>
         <HeaderBanner title={localeString('settings.title')}/>
         <ScrollView 
-            style={{height: '100%', marginLeft:'10%', marginRight:'10%'}}
-            contentInsetAdjustmentBehavior="automatic">
-            <View>
-                <Text style={[AppStyle.sectionTitle]}>
-                    {localeString('settings.userInterface.title')}
-                </Text>
-            </View>
-            <Separator/>
-            <ListItem noIndent noBorder>
+          style={{height: '100%', marginLeft:'10%', marginRight:'10%'}}
+          contentInsetAdjustmentBehavior="automatic">
+          <View>
+            <Text style={[AppStyle.sectionTitle]}>
+              {localeString('settings.userInterface.title')}
+            </Text>
+          </View>
+          <Separator/>
+          <ListItem noIndent noBorder>
             <Left>
-                <Text>Deutch</Text>
-              </Left>
-              <Right>
-                <Icon name="arrow-forward" />
-              </Right>
-            </ListItem>
-            <View style={{height:25}}>
-    
-            </View>
-            <View>
-                <Text style={[AppStyle.sectionTitle,{marginBottom: 5}]}>
-                    {localeString('settings.notifications.title')}
-                </Text>
-                <Text style={[AppStyle.textQuestion]}>
-                  {localeString('settings.notifications.notificationsInformation')}
-                </Text>
-            </View>
-    
-            <ListItem noIndent>
-              <Left>
-                <Text>
-                  {localeString('settings.notifications.notificationActivation')}
-                </Text>
-              </Left>
-              <Right>
-                <Switch/>
-              </Right>
-            </ListItem>
-            
-            <View style={{height:'40%'}}>
-    
-            </View>
-            
-            <View>
-                <Text style={[AppStyle.sectionTitle,{marginBottom: 5}]}>
-                  {localeString('settings.other.title')}
-                </Text>
-                <Separator />
-    
-            <Grid>
-              <Row>
-                <Col style={[styles.columns]}>
-                  <Button style={[AppStyle.button]}
-                    onPress={() => this.goToImpressum()} >
-                    <Text style={[AppStyle.textButton]}>{localeString('settings.other.impressum')}</Text>
-                  </Button>
-                </Col>
-                <Col style={[styles.columns]}>
-                  <Button style={[AppStyle.button]}>
-                    <Text style={[AppStyle.textButton]}>{localeString('settings.other.termsOfUse')}</Text>
-                  </Button>
-                </Col>
-              </Row>
-              <Row>
-                <Col style={[styles.columns]}>
-                  <Button style={[AppStyle.button]}>
-                    <Text style={[AppStyle.textButton]}>{localeString('settings.other.privacyPolicy')}</Text>
-                  </Button>
-                </Col>
-                <Col style={[styles.columns]}>
-                  <Button style={[AppStyle.button]}>
-                    <Text style={[AppStyle.textButton]}>{localeString('settings.other.feedback')}</Text>
-                  </Button>
-                </Col>
-              </Row>
-            </Grid>
-            </View>
+              <Text>Deutch</Text>
+            </Left>
+            <Right>
+              <Icon name="arrow-forward" />
+            </Right>
+          </ListItem>
+          <View style={{height:25}}>  
+          </View>
+          <View>
+              <Text style={[AppStyle.sectionTitle,{marginBottom: 5}]}>
+                  {localeString('settings.notifications.title')}
+              </Text>
+              <Text style={[AppStyle.textQuestion]}>
+                {localeString('settings.notifications.notificationsInformation')}
+              </Text>
+          </View>
+
+          <ListItem noIndent>
+            <Left>
+              <Text>
+                {localeString('settings.notifications.notificationActivation')}
+              </Text>
+            </Left>
+            <Right>
+              <Switch/>
+            </Right>
+          </ListItem>
+          
+          <View style={{height:'40%'}}>
+          </View>
+          
+          <View>
+            <Text style={[AppStyle.sectionTitle,{marginBottom: 5}]}>
+              {localeString('settings.other.title')}
+            </Text>
             <Separator />
-            <View>
+            <Grid>
+            <Row>
+              <Col style={[styles.columns]}>
+                <Button style={[AppStyle.button]}
+                  onPress={() => this.goToImpressum()} >
+                  <Text style={[AppStyle.textButton]}>
+                    {localeString('settings.other.impressum')}
+                  </Text>
+                </Button>
+              </Col>
+              <Col style={[styles.columns]}>
+                <Button style={[AppStyle.button]}>
+                  <Text style={[AppStyle.textButton]}>{localeString('settings.other.termsOfUse')}</Text>
+                </Button>
+              </Col>
+            </Row>
+            <Row>
+              <Col style={[styles.columns]}>
+                <Button style={[AppStyle.button]}>
+                  <Text style={[AppStyle.textButton]}>{localeString('settings.other.privacyPolicy')}</Text>
+                </Button>
+              </Col>
+              <Col style={[styles.columns]}>
+                <Button style={[AppStyle.button]}>
+                  <Text style={[AppStyle.textButton]}>{localeString('settings.other.feedback')}</Text>
+                </Button>
+              </Col>
+            </Row>
+          </Grid>
+          </View>
+          <Separator />
+          <View>
               <Button style={[AppStyle.button, {backgroundColor:colors.secondaryNormal}]}>
                 <Text style={[AppStyle.textButton,{color:colors.white}]}>
                   {localeString('settings.donate')}
-                </Text>
-                  </Button>
-                </Col>
-              </Row>
-            </Grid>
-            </View>
-            <Separator />
-            <View>
-              <Button style={[AppStyle.button, {backgroundColor:colors.secondaryNormal}]}>
-                <Text style={[AppStyle.textButton,{color:colors.white}]}>
-                  Ich möchte spenden!
                 </Text>
               </Button>
             </View>
